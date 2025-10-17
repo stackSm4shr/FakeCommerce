8ac8df3609ccc306cab5a7297ce3f1c86d039d7e
Tim <timbecker1988@gmail.com>
2025-10-17 12:59:42 +0200

    FetchProduct component added

---
+++ src/components/FetchProduct.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+export function FetchProducts() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return { products, loading };
+}
